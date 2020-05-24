d00e749ccbde3325f0714c568b97cc9fbd6b63b5
Paul Rutledge <rutledgepaulv@gmail.com>
2020-05-23 22:46:03 -0500

    cleanup

--- repl/ui_kit/forms.cljs
+++ repl/ui_kit/forms.cljs
@@ -6,17 +6,32 @@
             [ui-kit.core :as ui]))
 
 
-(def Address
-  [:map {:attrs 1}
-   [:id string?]
-   [:tags [:set keyword?]]
-   [:address
-    [:map
-     [:street string?]
-     [:city string?]
-     [:zip int?]
-     [:lonlat [:tuple double? double?]]]]])
+(defcard-rg string-field
+  [ui/schema->form string?])
 
+(defcard-rg int-field
+  [ui/schema->form int?])
+
+(defcard-rg boolean-field
+  [ui/schema->form boolean?])
+
+(defcard-rg enum-field
+  [ui/schema->form [:enum "Dogs" "Cats" "Birds"]])
+
+(defcard-rg tuple
+  [ui/schema->form [:tuple string? string?]])
 
 (defcard-rg address-form
-  [ui/schema->form Address])
+  [ui/schema->form
+   [:map
+    [:street string?]
+    [:city string?]
+    [:zip int?]
+    [:state [:enum "Illinois" "Minnesota"]]]])
+
+(defcard-rg data->form
+  [ui/data->form
+   {:street "131 W Herring Drive"
+    :city   "Chicago"
+    :zip    60632
+    :state  "Illinois"}])
--- src/cljs/ui_kit/core.cljs
+++ src/cljs/ui_kit/core.cljs
@@ -1,38 +1,65 @@
 (ns ui-kit.core
   (:require [ui-kit.semantic :as sa]
+            [malli.provider :as mp]
             [malli.core :as m]))
 
 
 (defn dispatch-fun [form]
-  (println form)
   (:name form))
 
 (defmulti compile-form
   (fn [& args] (apply dispatch-fun args)))
 
-(defmethod compile-form :default [schema children path options]
+(defmethod compile-form :default [{:keys [name] :as node}]
+  node)
+
+(defmethod compile-form :and [{:keys [name] :as node}]
   )
 
-(defmethod compile-form :and [schema children path options]
+(defmethod compile-form :or [{:keys [name] :as node}]
   )
 
-(defmethod compile-form :or [schema children path options]
+(defmethod compile-form :map [{:keys [name children] :as node}]
+  [sa/form-group
+   (for [[index [attr props child]] (map-indexed vector children)]
+     [sa/form-field {:key index}
+      [:label attr]
+      child])])
+
+(defmethod compile-form :multi [{:keys [name] :as node}]
   )
 
-(defmethod compile-form :map [schema children path options]
-  )
+(defmethod compile-form :enum [{:keys [name path children] :as node}]
+  [sa/form-select
+   {:options (for [child children]
+               {:key child :value child :text child})}])
 
-(defmethod compile-form :multi [schema children path options]
-  )
+(defmethod compile-form :tuple [{:keys [name children] :as node}]
+  [sa/form-group {:inline true :widths :equal}
+   (for [[index child] (map-indexed vector children)]
+     [sa/form-field {:key index} child])])
 
-(defmethod compile-form :enum [schema children path options]
-  )
+(defmethod compile-form :re [node]
+  [sa/form-input {:type :text}])
+
+(defmethod compile-form 'string? [node]
+  [sa/form-input {:type :text}])
+
+(defmethod compile-form 'int? [node]
+  [sa/form-input {:type :number}])
+
+(defmethod compile-form 'double? [node]
+  [sa/form-input {:type :number}])
+
+(defmethod compile-form 'boolean? [node]
+  [sa/form-input {:type :checkbox}])
 
 (defn compile-form-wrapper [schema children path options]
   (let [schema-name  (m/name schema)
         schema-props (m/properties schema)]
     (compile-form
       {:name     schema-name
+       :schema   schema
        :props    schema-props
        :children children
        :path     path
@@ -41,5 +68,9 @@
 (defn schema->form
   ([schema]
    (schema->form schema {}))
-  ([schema attrs]
-   [sa/form attrs (m/accept schema compile-form-wrapper)]))
+  ([schema data]
+   (m/accept schema compile-form-wrapper)))
+
+(defn data->form
+  ([data]
+   (schema->form (mp/provide [data]) data)))

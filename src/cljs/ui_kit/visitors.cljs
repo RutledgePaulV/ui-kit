fba254b0abc505f421a7808728d65966d59bc703
Paul Rutledge <rutledgepaulv@gmail.com>
2020-05-29 07:43:40 -0500

    cleanup grouping / prep context propagation for things like value transformation and validation

--- src/cljs/ui_kit/visitors.cljs
+++ src/cljs/ui_kit/visitors.cljs
@@ -10,18 +10,21 @@
 
     node - the malli schema node
     cursor - reagent cursor for that position in the form
-    attrs - any settings passed to a child from the parent
+    context - any settings passed to a child from the parent
 
    If node is not a leaf you're responsible for directly
    rendering or recursively visiting the children.
   "
-  (fn [node cursor attrs]
+  (fn [node cursor context]
     (malli/name node)))
 
-(defn is-required? [props attrs]
+(defn is-required? [props context]
   (if (contains? props :optional)
     (not (:optional props))
-    (or (:required attrs) true)))
+    (or (:required context) true)))
+
+(defn child-context [context & more]
+  (apply merge (select-keys context [:malli]) more))
 
 (defn schema-dissoc [map-schema key]
   (vec
@@ -47,21 +50,21 @@
      [:p message]
      [:code [:pre (utils/ppstr (m/form node))]]]]])
 
-(defmethod visit :default [node cursor attrs]
+(defmethod visit :default [node cursor context]
   [error-message node "Unsupported schema. Add a defmethod to ui-kit.visitors/visit to remedy."])
 
-(defmethod visit :and [node cursor attrs]
+(defmethod visit :and [node cursor context]
   (let [children (malli/children node)]
     (into [sa/form-group {}]
           (for [[index child] (map-indexed vector children)]
-            (with-meta [visit child cursor {}] {:key index})))))
+            (with-meta [visit child cursor (child-context context)] {:key index})))))
 
-(defmethod visit :or [node cursor attrs]
+(defmethod visit :or [node cursor context]
   (let [children        (malli/children node)
         props           (malli/properties node)
         selected-option (r/atom 0)]
     (fn []
-      [:div {}
+      [sa/form-group
        [sa/form-field
         [:label (or (:sui/label props) "Select variant")]
         [sa/dropdown
@@ -70,18 +73,17 @@
           :options   (for [index (range (count children))]
                        {:key index :value index :text (str "Option " index)})
           :onChange  #(reset! selected-option (.-value %2))}]]
-       [sa/form-group
-        [visit (nth children @selected-option) cursor {}]]])))
+       [visit (nth children @selected-option) cursor (child-context context)]])))
 
-(defmethod visit :map [node cursor attrs]
+(defmethod visit :map [node cursor context]
   (let [children (malli/children node)]
     (into [sa/form-group]
           (for [[k v] children]
             (let [label (utils/kebab->title k)]
-              (with-meta [visit v (r/cursor cursor [k]) {:label label}] {:key k}))))))
+              (with-meta [visit v (r/cursor cursor [k]) (child-context context {:label label})] {:key k}))))))
 
 
-(defmethod visit :multi [node cursor attrs]
+(defmethod visit :multi [node cursor context]
   (let [props    (malli/properties node)
         children (malli/children node)
         dispatch (get-in props [:dispatch])]
@@ -89,7 +91,7 @@
       (keyword? dispatch)
       (let [label (utils/kebab->title dispatch)
             by-dv (into {} (map (juxt first identity)) children)]
-        [:div {}
+        [sa/form-group
          [sa/form-field {:required (not (:optional props false))}
           [:label label]
           [sa/dropdown
@@ -100,26 +102,23 @@
                                  {:key dispatch-value :value dispatch-value :text dispatch-value}))
             :onChange     #(swap! cursor assoc dispatch (keyword (.-value %2)))}]]
          (when-some [child (get by-dv (get-in @cursor [dispatch]))]
-           (let [child-without-dispatch-field
-                 (schema-dissoc (second child) dispatch)]
-             [sa/form-group {}
-              [visit child-without-dispatch-field cursor {}]]))])
+           [visit (schema-dissoc (second child) dispatch) cursor (child-context context)])])
       :otherwise
       (error-message node "only multi schemas that dispatch by keyword or equality are supported."))))
 
-(defmethod visit :maybe [node cursor attrs]
-  [visit (first (malli/children node)) cursor (assoc attrs :required false)])
+(defmethod visit :maybe [node cursor context]
+  [visit (first (malli/children node)) cursor (assoc context :required false)])
 
 
 ; TODO: use fancier r/cursor with lens and lookup table of {row index} to avoid re-rendering
 ; each row each time.
-(defmethod visit :vector [node cursor attrs]
+(defmethod visit :vector [node cursor context]
   (let [[child-schema] (malli/children node)
         child-values (or @cursor [])]
     (conj (into [:div {}]
                 (for [index (range (count child-values))]
                   [sa/form-group {:inline true :key (random-uuid)}
-                   [visit child-schema (r/cursor cursor [index]) {}]
+                   [visit child-schema (r/cursor cursor [index]) (child-context context)]
                    [sa/button
                     {:icon     true
                      :disabled (= 0 index)
@@ -134,37 +133,35 @@
                     {:icon     true
                      :on-click (fn [] (swap! cursor (fn [old] (utils/vec-remove old index))))}
                     [sa/icon {:name "delete" :size :small}]]]))
-          [sa/button {:icon     true
-                      :on-click (fn [] (swap! cursor
-                                              (fn [old]
-                                                (let [default (get-default-value child-schema)]
-                                                  (conj old default)))))}
+          [sa/button
+           {:icon     true
+            :on-click (fn [] (swap! cursor (fn [old] (conj old (get-default-value child-schema)))))}
            [sa/icon {:name "plus" :size :large}]])))
 
-(defmethod visit :tuple [node cursor attrs]
+(defmethod visit :tuple [node cursor context]
   (into [sa/form-group {:inline true}]
         (for [[index child] (map-indexed vector (malli/children node))]
-          [visit child (r/cursor cursor [index]) {}])))
+          [visit child (r/cursor cursor [index]) (child-context context)])))
 
-(defmethod visit := [node cursor attrs]
-  (let [props     (malli/properties node)
-        all-attrs (utils/select-ns props :sui)
-        value     (or (deref cursor) (:default props))
-        label     (or (:label all-attrs) (:label attrs))
-        required  (is-required? props attrs)]
+(defmethod visit := [node cursor context]
+  (let [props       (malli/properties node)
+        all-context (utils/select-ns props :sui)
+        value       (or (deref cursor) (:default props))
+        label       (or (:label all-context) (:label context))
+        required    (is-required? props context)]
     [sa/form-field
-     (merge {:required required} (dissoc all-attrs :label))
+     (merge {:required required} (dissoc all-context :label))
      (when (some? label) [:label label])
      [sa/form-input {:value value :disabled true}]]))
 
-(defmethod visit :enum [node cursor attrs]
-  (let [props     (malli/properties node)
-        all-attrs (utils/select-ns props :sui)
-        value     (or (deref cursor) (:default props))
-        label     (or (:label all-attrs) (:label attrs))
-        required  (is-required? props attrs)]
+(defmethod visit :enum [node cursor context]
+  (let [props       (malli/properties node)
+        all-context (utils/select-ns props :sui)
+        value       (or (deref cursor) (:default props))
+        label       (or (:label all-context) (:label context))
+        required    (is-required? props context)]
     [sa/form-field
-     (merge {:required required} (dissoc all-attrs :label))
+     (merge {:required required} (dissoc all-context :label))
      (when (some? label) [:label label])
      [sa/dropdown
       {:selection    true
@@ -174,14 +171,14 @@
                             {:key child :value child :text child}))
        :onChange     #(reset! cursor (.-value %2))}]]))
 
-(defn simple-leaf [node cursor attrs mixins]
-  (let [props     (malli/properties node)
-        all-attrs (utils/select-ns props :sui)
-        value     (or (deref cursor) (:default props))
-        label     (or (:label all-attrs) (:label attrs))
-        required  (is-required? props attrs)]
+(defn simple-leaf [node cursor context mixins]
+  (let [props       (malli/properties node)
+        all-context (utils/select-ns props :sui)
+        value       (or (deref cursor) (:default props))
+        label       (or (:label all-context) (:label context))
+        required    (is-required? props context)]
     [sa/form-field
-     (merge {:required required} (dissoc all-attrs :label))
+     (merge {:required required} (dissoc all-context :label))
      (when (some? label) [:label label])
      [sa/form-input
       (merge {:defaultValue (or value "")
@@ -189,23 +186,23 @@
               :on-blur      #(reset! cursor (utils/event->value %))}
              mixins)]]))
 
-(defmethod visit ':re [node cursor attrs]
-  (simple-leaf node cursor attrs {:type :text}))
+(defmethod visit ':re [node cursor context]
+  (simple-leaf node cursor context {:type :text}))
 
-(defmethod visit 'string? [node cursor attrs]
-  (simple-leaf node cursor attrs {:type :text}))
+(defmethod visit 'string? [node cursor context]
+  (simple-leaf node cursor context {:type :text}))
 
-(defmethod visit 'int? [node cursor attrs]
-  (simple-leaf node cursor attrs {:type :number}))
+(defmethod visit 'int? [node cursor context]
+  (simple-leaf node cursor context {:type :number}))
 
-(defmethod visit 'pos-int? [node cursor attrs]
-  (simple-leaf node cursor attrs {:type :number}))
+(defmethod visit 'pos-int? [node cursor context]
+  (simple-leaf node cursor context {:type :number}))
 
-(defmethod visit 'boolean? [node cursor attrs]
-  (simple-leaf node cursor attrs {:type :checkbox}))
+(defmethod visit 'boolean? [node cursor context]
+  (simple-leaf node cursor context {:type :checkbox}))
 
-(defmethod visit 'inst? [node cursor attrs]
-  (simple-leaf node cursor attrs {:type :datetime-local}))
+(defmethod visit 'inst? [node cursor context]
+  (simple-leaf node cursor context {:type :datetime-local}))
 
-(defmethod visit 'uri? [node cursor attrs]
-  (simple-leaf node cursor attrs {:type :url}))
+(defmethod visit 'uri? [node cursor context]
+  (simple-leaf node cursor context {:type :url}))
